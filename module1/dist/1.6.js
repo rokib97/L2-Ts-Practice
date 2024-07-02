e048cb9bd5ace198cd0f97641f1982a817095f08
rokib97 <hasanrokib1997@gmail.com>
2024-07-02 15:49:09 +0600

    destructuring

--- module1/dist/1.6.js
+++ module1/dist/1.6.js
@@ -2,7 +2,7 @@
 // learning function
 // normal function
 function addTwoNumber(num1, num2 = 10) {
-  return num1 + num2;
+    return num1 + num2;
 }
 addTwoNumber(10, 20);
 // arrow function
@@ -10,16 +10,23 @@ const addTwoNumArr = (num1, num2) => num1 + num2;
 addTwoNumArr(40, 50);
 // object => function => method
 const poorUser = {
-  name: "rokib",
-  lastName: "hasan",
-  balance: 0,
-  addBalance(balance) {
-    return `My total balance is ${this.balance + balance}`;
-  },
-  fullName: function (first, last) {
-    return `my full name is ${this.name + this.lastName}`;
-  },
+    name: "rokib",
+    lastName: "hasan",
+    balance: 0,
+    addBalance(balance) {
+        return `My total balance is ${this.balance + balance}`;
+    },
+    fullName: function (first, last) {
+        return `my full name is ${this.name + this.lastName}`;
+    },
 };
 // mapping through an array
 let newNumbers = [2, 4, 6, 8, 20];
 const newArray = newNumbers.map((number) => number * number);
+// filter
+const filteredArray = newNumbers.filter((number) => number % 2 == 0);
+// find
+const singleElement = newNumbers.find((number) => number % 5 == 0);
+/**
+ * !Understanding TypeScript is like understanding your ex in a better way!
+ */
