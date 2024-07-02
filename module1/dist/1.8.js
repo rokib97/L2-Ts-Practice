2ce0ee93c640e9433219b7c15f229e5dc15a2adf
rokib97 <hasanrokib1997@gmail.com>
2024-07-02 17:26:24 +0600

    type alias

--- module1/dist/1.8.js
+++ module1/dist/1.8.js
@@ -1,22 +1,19 @@
 "use strict";
 {
-  //object destructuring
-  const user = {
-    id: 1,
-    name: {
-      firstName: "Rokibul",
-      middleName: "Hasan",
-      lastName: "Rokib",
-    },
-    contact: "01799999999",
-    address: "221b",
-  };
-  const {
-    id,
-    name: { firstName: prothomNam, lastName },
-    address: thikana, // name alias
-  } = user;
-  //   array destructuring
-  const friends = ["tonmoy", "rakib", "milon", "tota", "sabrina", "moni"];
-  const [, , bestFriend, ...rest] = friends;
+    //object destructuring
+    const user = {
+        id: 1,
+        name: {
+            firstName: "Rokibul",
+            middleName: "Hasan",
+            lastName: "Rokib",
+        },
+        contact: "01799999999",
+        address: "221b",
+    };
+    const { id, name: { firstName: prothomNam, lastName }, address: thikana, // name alias
+     } = user;
+    //   array destructuring
+    const friends = ["tonmoy", "rakib", "milon", "tota", "sabrina", "moni"];
+    const [, , bestFriend, ...rest] = friends;
 }
---
+++ module1/src/1.9.ts
@@ -0,0 +1,41 @@
+{
+  // Tyle Alias
+  // for object
+  type Student = {
+    name: string;
+    age: number;
+    prof: string;
+    address: string;
+    contactNo?: string;
+  };
+  const student1: Student = {
+    name: "rokib",
+    age: 21,
+    prof: "Web Dev",
+    address: "Dhk",
+    contactNo: "0169999999",
+  };
+  const student2: Student = {
+    name: "saki",
+    age: 17,
+    prof: "Web Dev",
+    address: "ctg",
+    contactNo: "01699999999",
+  };
+  const student3: Student = {
+    name: "rakib",
+    age: 26,
+    prof: "Teacher",
+    address: "Cumilla",
+  };
+
+  //   for string and  boolean
+  type UserName = string;
+  type IsAdmin = boolean;
+  const userName: UserName = "Rokib";
+  const isAdmin: IsAdmin = true;
+
+  //   type alias for function
+  type AddTwoNum = (num1: number, num2: number) => number;
+  const addTwoNum: AddTwoNum = (num1, num2) => num1 + num2;
+}
