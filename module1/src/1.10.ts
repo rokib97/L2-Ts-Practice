8ec06cd00940d5747331ca42b44086e58451b3c7
rokib97 <hasanrokib1997@gmail.com>
2024-07-02 20:35:34 +0600

    union and intersection type

---
+++ module1/src/1.10.ts
@@ -0,0 +1,37 @@
+{
+  // Union type
+  type FrontendDeveloper = "fakibuzDeveloper" | "juniorDeveloper";
+  type FullStackDeveloper = "frontendDeveloper" | "expartDeveloper";
+
+  type Developer = FrontendDeveloper | FullStackDeveloper;
+  const newDeveloper: Developer = "frontendDeveloper";
+
+  type User = {
+    name: string;
+    email?: string;
+    gender: "male" | "female"; //string literal with union
+    bloodGroup: "O+" | "A+" | "AB+" | "O-";
+  };
+  const user1: User = {
+    name: "rokib",
+    gender: "male",
+    bloodGroup: "O+",
+  };
+
+  //   intersection type
+  type Bowler = {
+    skills: string[];
+    designation1: "Bowler";
+  };
+  type Batsman = {
+    skills: string[];
+    designation2: "Batsman";
+  };
+  type AllRounder = Bowler & Batsman;
+
+  const allRounder: AllRounder = {
+    skills: ["rightArm", "googly", "coverDrive", "360shot"],
+    designation1: "Bowler",
+    designation2: "Batsman",
+  };
+}
